c162470789740e53af1f66753de0b606f892e6e9
santino <santinochang@auforce.com>
2024-06-25 18:53:35 +0800

    feat: fix

--- server.js
+++ server.js
@@ -11,28 +11,52 @@
 //   console.log('listening on port %s...', server.address().port);
 // });
 
-const express = require('express');
-const ServerSocket  = require('ws').Server
-const PORT  = 8081;
-const server = express()
-    .listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+// const express = require('express');
+// const ServerSocket  = require('ws').Server
+// const PORT  = 8081;
+// const server = express()
+//     .listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+//
+// const wss =  new ServerSocket ({ server })
+//
+// wss.on('connection', (ws ,req) => {
+//   console.log('[Client connected]')
+//   ws.id = req.headers['sec-websocket-key'].substring(0, 8)
+//   console.log( req.headers)
+//   ws.send(`${ws.id}`)
+//   ws.on('message', message => {
+//     let clients = wss.clients
+//     clients.forEach(client => {
+//       client.send( ` ${ws.id} : ${message}`)
+//
+//     })
+//   })
+//   // Connection closed
+//   ws.on('close', () => {
+//     console.log('Close connected')
+//   })
+// })
 
-const wss =  new ServerSocket ({ server })
 
-wss.on('connection', (ws ,req) => {
-  console.log('[Client connected]')
-  ws.id = req.headers['sec-websocket-key'].substring(0, 8)
-  console.log( req.headers)
-  ws.send(`${ws.id}`)
-  ws.on('message', message => {
-    let clients = wss.clients
-    clients.forEach(client => {
-      client.send( ` ${ws.id} : ${message}`)
 
-    })
-  })
-  // Connection closed
-  ws.on('close', () => {
-    console.log('Close connected')
-  })
+const {Server} = require("socket.io");
+const io = new Server({
+    cors: {
+        origin: "http://localhost:8081",
+        methods: ["GET", "POST"]
+    }
+
 })
+io.on("connection", (socket) => {
+    console.log('a user connected');
+
+    socket.on('message', (msg) => {
+        console.log(msg);
+        io.emit('message', socket.id + ':'+ msg );
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+io.listen(8081);
