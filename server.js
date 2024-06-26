a37fd501e917147ebe3db41bfd0513972ae482af
santino <santinochang@auforce.com>
2024-06-26 14:13:44 +0800

    feat: fix

--- server.js
+++ server.js
@@ -11,34 +11,6 @@
 //   console.log('listening on port %s...', server.address().port);
 // });
 
-// const express = require('express');
-// const ServerSocket  = require('ws').Server
-// const PORT  = 8081;
-// const server = express()
-//     .listen(PORT, () => console.log(`Server is running on port ${PORT}`))
-//
-// const wss =  new ServerSocket ({ server })
-//
-// wss.on('connection', (ws ,req) => {
-//   console.log('[Client connected]')
-//   ws.id = req.headers['sec-websocket-key'].substring(0, 8)
-//   console.log( req.headers)
-//   ws.send(`${ws.id}`)
-//   ws.on('message', message => {
-//     let clients = wss.clients
-//     clients.forEach(client => {
-//       client.send( ` ${ws.id} : ${message}`)
-//
-//     })
-//   })
-//   // Connection closed
-//   ws.on('close', () => {
-//     console.log('Close connected')
-//   })
-// })
-
-
-
 const {Server} = require("socket.io");
 const io = new Server({
     cors: {
@@ -48,11 +20,9 @@ const io = new Server({
 
 })
 io.on("connection", (socket) => {
-    console.log('a user connected');
-
     socket.on('message', (msg) => {
         console.log(msg);
-        io.emit('message', socket.id + ':'+ msg );
+        io.emit('message', socket.id + ':' + msg );
     });
 
     socket.on('disconnect', () => {
